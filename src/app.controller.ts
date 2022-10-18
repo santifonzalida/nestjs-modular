c3cd41a23162255195ee0c6a736733f1398c3794
santifonzalida <santiifonzalida@gmail.com>
2022-10-18 09:12:18 -0300

    decorator UseGuards and SetMetadata('isPublic')

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, UseGuards, SetMetadata } from '@nestjs/common';
 
+import { AppService } from './app.service';
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @SetMetadata('isPublic', true)
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @UseGuards(ApiKeyGuard)
   @Get('/task/')
   getTasks() {
     return this.appService.getTasks();
--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -4,14 +4,21 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get('isPublic', context.getHandler()); // read context
+    if (isPublic) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Auth');
     const isAuth = authHeader === '1234';
