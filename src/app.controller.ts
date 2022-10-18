e81fcfd960eab33aab26f5e9851e7082c625a0bc
santifonzalida <santiifonzalida@gmail.com>
2022-10-18 09:23:29 -0300

    New decorator Public to set public an Api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, UseGuards, SetMetadata } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { Public } from './auth/decorators/public.decorator';
 
 @UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @SetMetadata('isPublic', true)
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
